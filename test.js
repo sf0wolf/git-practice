7833f768fcbd9ce9e9bb226e56f6ac8fe577833f
sf0wolf <sf0wolf@gmail.com>
2023-05-18 13:23:33 +0900

    추가 작업

--- test.js
+++ test.js
@@ -3,4 +3,18 @@ const p = {
 	age: 24,
 };
 
-console.log(p);
+// 어떤자료형을 불리언으로 변환
+let v = 0;
+let b = !!v;
+console.log(b, typeof b);
+
+// 객체를 이용해서 switch문처럼 사용하기
+const direction = "northd";
+const dirKo = {
+	north: "북",
+	south: "남",
+	east: "동",
+	west: "서",
+};
+const r = dirKo[direction] ?? "무효";
+console.log(r);
